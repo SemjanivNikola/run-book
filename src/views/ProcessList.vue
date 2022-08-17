83faf905d6d2042e90cc534104e63a050de033b7
Nikola <nikola.sem450@gmail.com>
2022-08-17 18:11:15 +0200

    Added list components

---
+++ src/views/ProcessList.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="table-scroll">
+                <table class="list-table">
+                    <thead>
+                        <tr>
+                            <th class="header-icon-placeholder"></th>
+                            <th class="show-sm">
+                                Naziv<br /><small>Voditelj</small>
+                            </th>
+                            <th class="show-lg">Naziv</th>
+                            <th class="show-lg">Voditelj</th>
+                            <th class="align-center show-lg">Odrađeno</th>
+                            <th class="w-150 show-lg">Pokrenuto</th>
+                            <th class="w-100">Ažurirano</th>
+                            <th class="header-icon-placeholder"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <process-list-item />
+                    </tbody>
+                </table>
+            </div>
+</template>
+
+<script>
+import ProcessListItem from "./ProcessListItem.vue";
+
+export default {
+    name: "ProcessList",
+    components: { ProcessListItem },
+};
+</script>
+
+<style scoped>
+.table-scroll {
+    width: 100%;
+    padding-bottom: 16px;
+    position: relative;
+    overflow-x: hidden;
+    overflow-y: scroll;
+}
+
+.list-table {
+    width: 100%;
+    text-align: left;
+    border-spacing: 0;
+}
+
+.list-table thead tr {
+    position: sticky;
+    top: 0;
+    background-color: #f2f2f2;
+    padding: 0 16px;
+}
+
+.list-table thead tr th {
+    font-size: 14px;
+    padding: 8px 0;
+    z-index: 100;
+}
+</style>
---
+++ src/views/ProcessListItem.vue
@@ -0,0 +1,81 @@
+<template>
+    <tr>
+        <td :colspan="isSmall ? 4 : 7">
+            <table>
+                <tr onclick="window.location=`process-detail.html`;">
+                    <td>
+                        <span class="table-icon-placeholder">
+                            <icon
+                                name="progress-clock"
+                                size="24"
+                                color="#000"
+                            />
+                        </span>
+                    </td>
+                    <td class="show-sm">
+                        Proces prijave prakse<br /><small
+                            >Nikola Tanković</small
+                        >
+                    </td>
+                    <td class="show-lg">Proces prijave prakse</td>
+                    <td class="w-250 show-lg">Nikola Tanković</td>
+                    <td class="align-center show-lg">22%</td>
+                    <td class="w-150 show-lg">12.02.2022</td>
+                    <td class="w-100">14.07.2022</td>
+                    <td>
+                        <span class="table-icon-placeholder">
+                            <icon name="info" size="24" color="#000" />
+                        </span>
+                    </td>
+                </tr>
+                <tr class="show-sm progress-bar-row">
+                    <td colspan="4" class="progress-bar-wrapper">
+                        <span class="progress-bar" style="width: 68%"></span>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</template>
+
+<script>
+import Icon from "./Icon.vue";
+
+export default {
+    name: "ProcessListItem",
+    components: { Icon },
+    data () {
+        return {
+            isSmall: false,
+        };
+    },
+    created () {
+        this.isSmall = window.innerWidth >= 756;
+    },
+};
+</script>
+
+<style scoped>
+tr.action-row {
+    cursor: pointer;
+    transition: background-color 400ms ease-out;
+}
+
+tr.has-border {
+    border-bottom: 1px solid #eee;
+}
+
+tr.action-row td {
+    padding: 16px 0;
+}
+
+.table-icon-placeholder {
+    display: block;
+    width: 24px;
+    height: 24px;
+}
+
+tr.action-row:hover {
+    background-color: #FFFFFF;
+}
+</style>
