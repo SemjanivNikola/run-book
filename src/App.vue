bf1d2c48837ee5098cbc5c0d4cf4a566fc9dc532
Nikola <nikola.sem450@gmail.com>
2022-08-17 18:11:40 +0200

    Updated App component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-    <router-view />
+    <div id="app">
+        <router-view />
+    </div>
 </template>
 
 <script>
-
 export default {
     name: "App",
 };
@@ -11,8 +12,22 @@ export default {
 
 <style>
 #app {
+    position: relative;
+    height: 100vh;
+    max-width: 100vw;
+    overflow: hidden;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+}
+.icon-placeholder {
+    width: 24px;
+    height: 24px;
+}
+.spacer-md {
+    height: 16px;
+    width: 16px;
+    min-width: 16px;
+    min-height: 16px;
 }
 </style>
