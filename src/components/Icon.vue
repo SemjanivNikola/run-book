82b4419140166505ab43ed4d43cf34e4f4b09a93
Nikola <nikola.sem450@gmail.com>
2022-08-17 08:24:25 +0200

    Added icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,114 @@
+<!-- eslint-disable vue/no-v-html -->
+<template>
+    <div
+        id="icon-wrapper"
+        :style="{ width: size + 'px', height: size + 'px' }"
+        v-html="iconName()"
+    />
+</template>
+
+<script>
+export default {
+    name: "Icon",
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        size: {
+            type: Number,
+            required: false,
+            default: 24,
+        },
+        color: {
+            type: String,
+            required: false,
+            default: "rgba(255, 255, 255)",
+        },
+    },
+    computed: {
+        plus () {
+            return `<svg viewBox="0 0 16 16" fill="none">
+        <path fill="${this.color}" d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+        <path fill="${this.color}" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+      </svg>`;
+        },
+        close () {
+            return `<svg viewBox="0 0 24 24" fill="none">
+        <path fill="${this.color}" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+      </svg>`;
+        },
+        menu () {
+            return `<svg viewBox="0 0 16 16" fill="none">
+        <path fill="${this.color}" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+      </svg>`;
+        },
+        add () {
+            return `<svg viewBox="0 0 16 16" fill="none">
+        <path fill="${this.color}" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+      </svg>`;
+        },
+        file () {
+            return `<svg viewBox="0 0 16 16" fill="none">
+        <path fill="${this.color}" d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"/>
+      </svg>`;
+        },
+        search () {
+            return `<svg viewBox="0 0 24 24">
+                        <path fill="${this.color}"
+                            d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"/>
+                    </svg>">`;
+        },
+        progressClock () {
+            return `<svg viewBox="0 0 24 24">
+                        <path fill="${this.color}"
+                            d="M13,2.03V2.05L13,4.05C17.39,4.59 20.5,8.58 19.96,12.97C19.5,16.61 16.64,19.5 13,19.93V21.93C18.5,21.38 22.5,16.5 21.95,11C21.5,6.25 17.73,2.5 13,2.03M11,2.06C9.05,2.25 7.19,3 5.67,4.26L7.1,5.74C8.22,4.84 9.57,4.26 11,4.06V2.06M4.26,5.67C3,7.19 2.25,9.04 2.05,11H4.05C4.24,9.58 4.8,8.23 5.69,7.1L4.26,5.67M2.06,13C2.26,14.96 3.03,16.81 4.27,18.33L5.69,16.9C4.81,15.77 4.24,14.42 4.06,13H2.06M7.1,18.37L5.67,19.74C7.18,21 9.04,21.79 11,22V20C9.58,19.82 8.23,19.25 7.1,18.37M12.5,7V12.25L17,14.92L16.25,16.15L11,13V7H12.5Z"/>
+                    </svg>`;
+        },
+        info () {
+            return `<svg viewBox="0 0 24 24">
+                        <path fill="${this.color}"
+                            d="M13.5,4A1.5,1.5 0 0,0 12,5.5A1.5,1.5 0 0,0 13.5,7A1.5,1.5 0 0,0 15,5.5A1.5,1.5 0 0,0 13.5,4M13.14,8.77C11.95,8.87 8.7,11.46 8.7,11.46C8.5,11.61 8.56,11.6 8.72,11.88C8.88,12.15 8.86,12.17 9.05,12.04C9.25,11.91 9.58,11.7 10.13,11.36C12.25,10 10.47,13.14 9.56,18.43C9.2,21.05 11.56,19.7 12.17,19.3C12.77,18.91 14.38,17.8 14.54,17.69C14.76,17.54 14.6,17.42 14.43,17.17C14.31,17 14.19,17.12 14.19,17.12C13.54,17.55 12.35,18.45 12.19,17.88C12,17.31 13.22,13.4 13.89,10.71C14,10.07 14.3,8.67 13.14,8.77Z"/>
+                    </svg>`;
+        },
+    },
+    methods: {
+        iconName () {
+            switch (this.name) {
+                case "plus": {
+                    return this.plus;
+                }
+                case "close": {
+                    return this.close;
+                }
+                case "menu": {
+                    return this.menu;
+                }
+                case "add": {
+                    return this.add;
+                }
+                case "search": {
+                    return this.search;
+                }
+                case "progress-clock": {
+                    return this.progressClock;
+                }
+                case "info": {
+                    return this.info;
+                }
+                default: {
+                    // file
+                    return this.file;
+                }
+            }
+        },
+    },
+};
+</script>
+
+<style scoped>
+#icon-wrapper svg {
+    width: 100%;
+    height: 100%;
+}
+</style>
