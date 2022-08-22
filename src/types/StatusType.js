27ab044dfc81739ce3631618420e467856c1ba0a
Nikola <nikola.sem450@gmail.com>
2022-08-22 13:52:53 +0200

    Added status type

---
+++ src/types/StatusType.js
@@ -0,0 +1,8 @@
+const StatusType = [
+    { id: 1, type: "done" },
+    { id: 2, type: "in progress" },
+    { id: 3, type: "on hold" },
+    { id: 4, type: "error" },
+];
+
+export default Object.freeze(StatusType);
