106860dd16aea61a99e048f34812cfa2defa2d26
Nikola <nikola.sem450@gmail.com>
2022-10-02 21:24:31 +0200

    Added types and interfaces for store

--- src/store/interfaces/store.ts
+++ src/store/interfaces/store.ts
@@ -1,28 +1,28 @@
 
 // ===============================
 // PROCESS STORE
-interface ActiveProcesList {
+interface ActiveProcessStore {
     state: {
         list: Array<ProcessListItem>;
-        recordPreview: ProcessListItem | null;
-        processDetail: Process | null;
+        detail: Process | null; // Set when reading process by id
     };
 };
 
 // ===============================
-// LIST ITEM
+// LIST ITEM - Present on list screen / right sheet
 interface ProcessListItem {
     id: number;
     title: string;
     manager: string;
     averageDuration: string;
     totalSteps: number;
-    startedAt: string;
-    UpdateddAt: string;
+    progress: number | null; // This is not present on available process list screen
+    startedAt: string | null; // This is not present on available process list screen
+    UpdateddAt: string | null; // This is not present on available process list screen
 }
 
 // ===============================
-// DETAIL ITEM
+// DETAIL ITEM - present on process detail screen
 interface Process {
         id: number;
         info: Info;
@@ -41,13 +41,16 @@ interface Info {
     stepDone: number;
 }
 
+/**
+ * @actionSummary Variables gathered along the process flow
+ */
 interface Step {
     id: number;
     title: string;
     caption: string;
     description: string;
     summary: string;
-    actionSummary: null;
+    actionSummary: Array<string> | null;
     status: StepStatus,
     urlIndexList: Array<[number, number]>;
 }
@@ -57,6 +60,8 @@ interface URL {
     url: string;
 }
 
+// ===============================
+// TYPES
 /**
  * - **ACTIVE** Currently in progress. Shown on active screen
  * - **ON-HOLD** It was started but paused in some point. Shown on active screen
@@ -67,10 +72,10 @@ interface URL {
 type ProcessStatus = "ACTIVE" | "ON-HOLD" | "COMPLETED" | "CANCELLED" | "AVAILABLE";
 
 /**
- * - **ACTIVE** Current step
- * - **COPLETED** Step which step group is passed and it's done successfully. Marked with green(rgb(0, 185, 0)) color
- * - **SKIPPED** Step which step group is passed, but it's was not done . Marked with green(rgb(0, 185, 0)) color
+ * - **ACTIVE** Current step. Marked with blue(rgb(0,212,255)) color
+ * - **COPLETED** Step which group is passed and it's done successfully. Marked with green(rgb(0, 185, 0)) color
+ * - **INACTIVE** Step which group is passed, but it's was not done or step not yet reached. Marked with gray(rgb(171, 180, 194)) color
  */
-type StepStatus = "ACTIVE" | "COMPLETED" | "SKIPPED";
+type StepStatus = "ACTIVE" | "COMPLETED" | "INACTIVE"; // "ACTIVE-INTERVENE" when user action is needed?
 
 type ProcessStoreType = "ACTIVE" | "AVAILABLE" | "ARCHIVED";
