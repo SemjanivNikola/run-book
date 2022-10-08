db333b8127ac5e160bb87329c1d38ef7650f4ea5
Nikola <nikola.sem450@gmail.com>
2022-10-08 11:43:12 +0200

    Updated animation on list items

--- src/views/ProcessListItem.vue
+++ src/views/ProcessListItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <tr class="list-item" style="position: relative" @click="routeToScreen">
+    <tr class="list-item" @click="routeToScreen">
         <td>
             <span class="table-icon-placeholder">
                 <icon name="progress-clock" color="#000" />
@@ -13,7 +13,7 @@
         <td class="align-center show-lg">{{ process.progress }}%</td>
         <td class="w-150 show-lg">{{process.startedAt}}</td>
         <td class="w-100">{{process.updatedAt}}</td>
-        <td @click.stop="onClick">
+        <td class="table-action" @click.stop="onClick">
             <span class="table-icon-placeholder">
                 <icon name="info" color="#000" />
             </span>
@@ -48,93 +48,5 @@ export default {
 };
 </script>
 
-<style scoped>
-tr.list-item {
-    cursor: pointer;
-    transition: background-color 400ms ease-out;
-    border-bottom: 1px solid #eee;
-}
-tr.list-item:hover {
-    background-color: #ffffff;
-}
-tr.list-item td {
-    padding: 16px 0;
-}
-.table-icon-placeholder {
-    display: block;
-    width: 24px;
-    height: 24px;
-}
-.progress-bar-row {
-    position: absolute;
-    width: 100%;
-    bottom: -1px;
-    left: 0;
-    padding: 0;
-    background: transparent;
-}
-.progress-bar-row .progress-bar {
-    height: 4px;
-    border-radius: 0 16px 16px 0;
-    background-image: linear-gradient(
-        145deg,
-        rgba(0, 255, 255, 1) 0%,
-        rgba(204, 255, 102, 1) 100%
-    );
-}
-.progress-bar-row .progress-bar.error {
-    background-image: linear-gradient(
-        145deg,
-        rgba(255, 0, 0, 1) 0%,
-        rgba(255, 204, 0, 1) 100%
-    );
-}
-tr td.align-center {
-    text-align: center;
-}
-tr td.w-100 {
-    width: 100px;
-}
-tr td.w-150 {
-    width: 150px;
-}
-tr td.w-200 {
-    width: 200px;
-}
-tr td.w-250 {
-    width: 250px;
-}
-
-@media (max-width: 756px) {
-    .list-table tbody tr td.show-lg {
-        display: none;
-    }
-    .list-table tbody tr td:first-child {
-        width: 46px;
-        padding-left: 8px;
-    }
-    .list-table tbody tr td:last-child {
-        width: 32px;
-    }
-    .progress-bar-row.show-sm {
-        display: block;
-    }
-}
-
-@media (min-width: 756px) {
-    .list-table tbody tr.show-sm,
-    .list-table tbody tr td.show-sm {
-        display: none;
-    }
-    .list-table tbody tr td:first-child {
-        width: 56px;
-        padding-left: 16px;
-    }
-    .list-table tbody tr td:last-child {
-        width: 40px;
-    }
-    .progress-bar-row.show-sm {
-        display: none;
-    }
-}
+<style scoped src="@/styles/ProcessList.css">
 </style>
--- src/views/available-process/ProcessListItem.vue
+++ src/views/available-process/ProcessListItem.vue
@@ -1,10 +1,10 @@
 <template>
-    <tr class="list-item" style="position: relative" @click="routeToScreen">
+    <tr class="list-item start" @click="routeToScreen">
         <td><span class="table-icon-placeholder"></span></td>
         <td class="show-sm">
             {{ process.title }}<br /><small>{{ process.manager }}</small>
         </td>
-        <td class="color show-lg">
+        <td class="mix-blend show-lg">
             <span>{{ process.title }}</span>
         </td>
         <td class="w-200 show-lg">{{ process.manager }}</td>
@@ -17,7 +17,7 @@
                 process.totalSteps
             }}</small>
         </td>
-        <td @click.stop="onClick">
+        <td class="table-action" @click.stop="onClick">
             <span class="table-icon-placeholder">
                 <icon name="info" color="#000" />
             </span>
@@ -49,173 +49,5 @@ export default {
 };
 </script>
 
-<style scoped>
-tr.list-item {
-    position: relative;
-    cursor: pointer;
-    transition: background-color 400ms ease-out;
-    border-bottom: 1px solid #eee;
-    background: #f9f9f9;
-    overflow: hidden;
-    z-index: 10;
-}
-tr.list-item td {
-    padding: 16px 0;
-}
-tr.list-item td.color {
-    color: #000;
-}
-tr.list-item td.color span {
-    display: inline;
-    position: relative;
-    color: #fff;
-    mix-blend-mode: difference;
-    z-index: 100;
-}
-tr.list-item td.color:after {
-    content: "Pokreni ➔";
-    margin-left: 24px;
-    padding: 8px;
-    border-radius: 16px;
-    color: #f9f9f9;
-    mix-blend-mode: lighten;
-}
-.table-icon-placeholder {
-    display: block;
-    width: 24px;
-    height: 24px;
-}
-.progress-bar-row {
-    position: absolute;
-    width: 100%;
-    bottom: -1px;
-    left: 0;
-    padding: 0;
-    background: transparent;
-}
-.progress-bar-row .progress-bar {
-    height: 4px;
-    border-radius: 0 16px 16px 0;
-    background-image: linear-gradient(
-        145deg,
-        rgba(0, 255, 255, 1) 0%,
-        rgba(204, 255, 102, 1) 100%
-    );
-}
-.progress-bar-row .progress-bar.error {
-    background-image: linear-gradient(
-        145deg,
-        rgba(255, 0, 0, 1) 0%,
-        rgba(255, 204, 0, 1) 100%
-    );
-}
-tr td.align-center {
-    text-align: center;
-}
-tr td.w-100 {
-    width: 100px;
-}
-tr td.w-150 {
-    width: 150px;
-}
-tr td.w-200 {
-    width: 200px;
-}
-tr td.w-250 {
-    width: 250px;
-}
-
-/* Table row - CALL TO ACTION: hover */
-tr.list-item td.color:before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 0%;
-    height: 100%;
-    background-color: #fff;
-    z-index: -2;
-}
-tr.list-item:after {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 12%;
-    height: 100%;
-    background-color: #000;
-    transform: translateX(-110%);
-    z-index: -1;
-}
-tr.list-item:hover:after {
-    animation: slider 450ms ease-out forwards;
-}
-tr.list-item:hover td.color:before {
-    animation: slider-line 150ms ease-out 150ms forwards;
-}
-
-@keyframes slider {
-    0% {
-        transform: skew(-18deg) translateX(-110%);
-    }
-    25% {
-        width: 100%;
-        transform: skew(-18deg) translateX(0);
-    }
-    50% {
-        width: 50%;
-        transform: skew(-18deg) translateX(202%);
-    }
-    55% {
-        width: 0%;
-        opacity: 0;
-        transform: skew(-18deg) translateX(-5%);
-    }
-    100% {
-        width: 37%;
-        opacity: 1;
-        transform: skew(-18deg) translateX(-5%);
-    }
-}
-@keyframes slider-line {
-    from {
-        width: 0%;
-    }
-    to {
-        width: 100%;
-    }
-}
-
-@media (max-width: 756px) {
-    .list-table tbody tr td.show-lg {
-        display: none;
-    }
-    .list-table tbody tr td:first-child {
-        width: 46px;
-        padding-left: 8px;
-    }
-    .list-table tbody tr td:last-child {
-        width: 32px;
-    }
-    .progress-bar-row.show-sm {
-        display: block;
-    }
-}
-
-@media (min-width: 756px) {
-    .list-table tbody tr.show-sm,
-    .list-table tbody tr td.show-sm {
-        display: none;
-    }
-    .list-table tbody tr td:first-child {
-        width: 56px;
-        padding-left: 16px;
-    }
-    .list-table tbody tr td:last-child {
-        width: 40px;
-    }
-    .progress-bar-row.show-sm {
-        display: none;
-    }
-}
+<style scoped src="@/styles/ProcessList.css">
 </style>
