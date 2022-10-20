69275767a39a0fff8b839f3fee975186cdd5e264
Nikola <nikola.sem450@gmail.com>
2022-10-20 21:15:40 +0200

    Update process diagram

--- src/components/ProcessDiagram.vue
+++ src/components/ProcessDiagram.vue
@@ -6,95 +6,29 @@
                 :key="index"
                 class="diagram-col"
             >
-                <div
-                    v-for="(step, innerIndex) in group"
+                <process-diagram-item
+                    v-for="step in group"
                     :key="step.id"
-                    :class="[{ current: step.isActive }, 'diagram-col-item']"
-                    title="Pregledaj"
-                    translate="yes"
-                >
-                    <div class="diagram-item-wrapper">
-                        <div class="diagram-item-content">
-                            <small v-if="isActive">Trenutni korak</small>
-                            <h4>
-                                {{ index + 1 }}.<span class="index-addon" v-if="group.length > 1">{{
-                                    subArray[innerIndex]
-                                }}</span>
-                                {{ step.title }}
-                            </h4>
-                            <div v-if="isActionNeeded" class="item-status">
-                                <icon name="warning" color="#e81e63" />
-                                <span>Potrebna akcija</span>
-                            </div>
-
-                            <!-- FAB WITH DESCRIPTION-->
-                            <button
-                                v-if="!isActionNeeded"
-                                type="button"
-                                :title="action.title"
-                                translate="yes"
-                                class="diagram-item-fab"
-                            >
-                                <div
-                                    class="fab-wrapper"
-                                    @click="$emit('on-action')"
-                                >
-                                    <span>{{ action.caption }}</span>
-                                    <div class="action-icon">
-                                        <icon
-                                            name="plus"
-                                            color="rgb(0,212,255)"
-                                            :size="28"
-                                        />
-                                    </div>
-                                </div>
-                            </button>
-                        </div>
-                    </div>
-                </div>
+                    :step="step"
+                    :index="index"
+                    :shouldCount="group.length > 1"
+                />
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import Icon from "./Icon.vue";
+import ProcessDiagramItem from "./ProcessDiagramItem.vue";
+
 export default {
-    components: { Icon },
+    components: { ProcessDiagramItem },
     name: "ProcessDiagram",
     props: {
         stepList: {
             type: Array,
             required: true,
         },
-    },
-    data () {
-        return {
-            isActive: false,
-            isActionNeeded: false,
-            action: {
-                title: "Odaberi preferencije",
-                caption: "Preferencije",
-            },
-            subArray: [
-                "a",
-                "b",
-                "c",
-                "d",
-                "e",
-                "f",
-                "g",
-                "h",
-                "i",
-                "j",
-                "k",
-                "l",
-                "m",
-                "n",
-                "o",
-                "p",
-            ],
-        };
     },
 };
 </script>
---
+++ src/components/ProcessDiagramItem.vue
@@ -0,0 +1,118 @@
+<template>
+    <div
+        :class="[{ current: isActive }, 'diagram-col-item']"
+        title="Pregledaj"
+        translate="yes"
+    >
+        <div class="diagram-item-wrapper">
+            <div class="diagram-item-content">
+                <small v-if="isActive">Trenutni korak</small>
+                <h4>
+                    {{ index + 1 }}.<span
+                        class="index-addon"
+                        v-if="shouldCount"
+                        >{{ subArray[step.id - 1] }}</span
+                    >
+                    {{ step.title }}
+                </h4>
+
+                <div v-if="isActionNeeded" class="action item-status">
+                    <icon name="warning" color="#e81e63" />
+                    <span>Potrebna akcija</span>
+                </div>
+                <!-- Hide on INACTIVE step -->
+                <div v-if="isActive && !isActionNeeded" class="item-status">
+                    <icon name="progress-clock" color="rgb(171, 180, 194)" />
+                    <span>Čekanje se odgovor treće strane</span>
+                </div>
+
+                <!-- FAB WITH DESCRIPTION-->
+                <button
+                    v-if="isActionNeeded"
+                    type="button"
+                    :title="step.action.title"
+                    translate="yes"
+                    class="diagram-item-fab"
+                >
+                    <div class="fab-wrapper" @click="$emit('on-action')">
+                        <span>{{ step.action.label }}</span>
+                        <div class="action-icon">
+                            <icon
+                                name="plus"
+                                color="rgb(0,212,255)"
+                                :size="28"
+                            />
+                        </div>
+                    </div>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Icon from "./Icon.vue";
+
+export default {
+    components: { Icon },
+    name: "ProcessDiagramItem",
+    props: {
+        step: {
+            type: Object,
+            required: true,
+        },
+        index: {
+            type: Number,
+            required: true,
+        },
+        shouldCount: {
+            type: Boolean,
+            required: true,
+        },
+    },
+    data () {
+        return {
+            isActive: false,
+            isActionNeeded: false,
+            subArray: [
+                "a",
+                "b",
+                "c",
+                "d",
+                "e",
+                "f",
+                "g",
+                "h",
+                "i",
+                "j",
+                "k",
+                "l",
+                "m",
+                "n",
+                "o",
+                "p",
+            ],
+        };
+    },
+    created () {
+        this.isActive = this.step.status === "ACTIVE";
+        this.isActionNeeded = Boolean(
+            this.isActive && this.step.action !== null,
+        );
+    },
+    computed: {
+        iconName () {
+            return this.isActionNeeded ? "" : "";
+        },
+        iconColor () {
+            return this.isActionNeeded ? "" : "";
+        },
+        iconTitle () {
+            return this.isActionNeeded ? "" : "";
+        },
+    },
+};
+</script>
+
+<style scoped src="./diagramStyle.css">
+</style>
