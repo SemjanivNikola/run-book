9d64f36c31716c74de6c81ef508d133041197a85
Nikola <nikola.sem450@gmail.com>
2023-03-04 18:20:48 +0100

    Update successfully submited form screen

--- src/screens/SuccessfulFormScreen.vue
+++ src/screens/SuccessfulFormScreen.vue
@@ -1,10 +1,7 @@
 <template>
     <div class="wrap">
-        <h1>ODLIČNO!</h1>
-        <div class="img-content">
-            <div class="img-wrap">
-                <img src="../assets/form-successful.svg" />
-            </div>
+        <div class="icon-wrap">
+             <icon :size="65" name="step-done" color="rgb(191, 240, 95) " />
         </div>
         <h3>Podaci su uspješno poslani</h3>
         <div class="inline">
@@ -22,7 +19,9 @@
 </template>
 
 <script>
+import Icon from "@/components/Icon.vue";
 export default {
+    components: { Icon },
     name: "SuccessfulFormScreen",
     data () {
         return {
@@ -31,6 +30,7 @@ export default {
     },
     created () {
         this.timeoutId = setTimeout(() => {
+            this.$store.commit("processStore/setShouldStartFlow", true);
             this.handleRoute();
         }, 5000);
     },
@@ -53,16 +53,6 @@ export default {
     gap: 8px;
     padding-top: 32px;
 }
-.wrap h1 {
-    font-size: 90px;
-    font-weight: 600;
-    font-family: "Niconne", cursive;
-    color: #e0d6e9;
-    transform-origin: center;
-    transform: rotate(-25deg) translate(4rem, -4rem);
-    text-shadow: 2px 2px 0px #957dad, 4px 4px 0px #ee4b2b, 6px 6px 0px #00c2cb,
-        8px 8px 0px #ff7f50, 10px 10px 0px #553c9a;
-}
 .wrap h3 {
     text-align: center;
 }
@@ -70,9 +60,8 @@ export default {
     width: 100%;
     margin-top: -90px;
 }
-.img-wrap {
-    max-width: 45%;
-    margin: 0 auto;
+.icon-wrap {
+    margin: 80px auto 45px auto;
 }
 .img-wrap img {
     width: 100%;
